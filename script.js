071e43b1aee5d873971f9c8979d3a922ea62a2c2
Aurimas <gaizauskasaurimas@gmail.com>
2022-07-13 11:17:38 +0500

    add realtime password feedback

--- script.js
+++ script.js
@@ -4,24 +4,67 @@ const passmsg = document.getElementById('passmsg');
 const incorrect = document.querySelectorAll('.pass + .incorrect')
 const correct = document.querySelectorAll('.pass ~ .correct')
 
+
+const match = document.getElementById('match');
+const lengthmsg = document.getElementById('length');
+const upperlower = document.getElementById('upperlower');
+const numsym = document.getElementById('numsym');
+
+
 correct.forEach(el => el.classList.add('hide'));
 
 pass1.addEventListener('input', handler);
 pass2.addEventListener('input', handler);
 
+
+function validator(msg,...args) {
+    let isTrue = true;
+    args.forEach(arg => {
+        if (!arg) isTrue = false;
+    })
+
+    const icon = document.querySelector(`#${msg.id} > i`);
+
+
+    if (isTrue) {
+        icon.classList.remove('hide')
+        msg.classList.add('green');
+    } else {
+        icon.classList.add('hide')
+        msg.classList.remove('green');
+    }
+}
+
 function handler (e) {
+
+    const num = /(?=.*[0-9])/g.test(pass1.value);
+    const symb = /(?=.*[@!#$%&()*+.])/g.test(pass1.value);
+    const lower = /(?=.*[a-z])/g.test(pass1.value);
+    const upper = /(?=.*[A-Z])/g.test(pass1.value);
+    const lenght = /.{8,}/g.test(pass1.value);
+
     pass1validity = pass1.validity.valid
     pass2Validity = pass2.validity.valid
 
-    if (pass1.value !== "" && pass1.value === pass2.value &&
-        pass1validity && pass2Validity
+    // if (pass1.value !== "" && pass1.value === pass2.value) {
+    //     // passmsg.classList.add('hide');
+
+    // } else {
+    //     passmsg.classList.remove('hide')
+    // }
+
+    validator(numsym, num, symb)
+    validator(lengthmsg, lenght)
+    validator(upperlower, upper, lower)
+
+    if (pass1validity && pass2Validity && 
+        pass1.value !== "" && pass1.value === pass2.value    
     ) {
-        passmsg.classList.add('hide');
+        match.classList.add('hide');
         incorrect.forEach(el => el.classList.add('hide'));
         correct.forEach(el => el.classList.remove('hide'));
-
     } else {
-        passmsg.classList.remove('hide')
+        match.classList.remove('hide');
         incorrect.forEach(el => el.classList.remove('hide'));
         correct.forEach(el => el.classList.add('hide'));
     }
